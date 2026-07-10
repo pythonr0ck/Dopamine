139639f3386517ea8884ffd25a8d7134c613d550
pelt <122493008+pythonr0ck@users.noreply.github.com>
2026-07-10 01:00:14 -0400

    Delete unused linker dummy stubs
    Removed unused linker dummy stubs for MachOMerger.

--- BaseBin/dyldhook/src/webkit_dsc_patch.c
+++ BaseBin/dyldhook/src/webkit_dsc_patch.c
@@ -295,28 +295,3 @@ bool HOOK(_ZNK5dyld416JustInTimeLoader11matchesPathEPKc)(const void *self, const
 
     return false;
 }
-
-// ============================================================================
-// Linker Dummy Stubs
-// MachOMerger needs these symbols to exist during the initial clang linking
-// phase. MachOMerger will overwrite these stubs with actual branches later.
-// ============================================================================
-.align 2
-.globl _MACHOMERGER_ORIG__ZNK5dyld416JustInTimeLoader11matchesPathEPKc
-_MACHOMERGER_ORIG__ZNK5dyld416JustInTimeLoader11matchesPathEPKc:
-    .space 16
-
-.align 2
-.globl _MACHOMERGER_ORIG__ZNK5dyld46Loader17applyCachePatchesERNS_12RuntimeStateERNS_34DyldCacheDataConstLazyScopedWriterE
-_MACHOMERGER_ORIG__ZNK5dyld46Loader17applyCachePatchesERNS_12RuntimeStateERNS_34DyldCacheDataConstLazyScopedWriterE:
-    .space 16
-
-.align 2
-.globl _MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader11loadAddressERNS_12RuntimeStateE
-_MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader11loadAddressERNS_12RuntimeStateE:
-    .space 16
-
-.align 2
-.globl _MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader21overridesDylibInCacheERPKNS0_10DylibPatchERt
-_MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader21overridesDylibInCacheERPKNS0_10DylibPatchERt:
-    .space 16
