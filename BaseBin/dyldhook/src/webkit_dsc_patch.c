7a975a7937256c101011aae9d947214707019caa
pelt <122493008+pythonr0ck@users.noreply.github.com>
2026-07-09 21:48:31 -0400

    Add Linker Dummy Stubs
    MachOMerger needs these symbols to exist during initial clang linking phase

--- BaseBin/dyldhook/src/webkit_dsc_patch.c
+++ BaseBin/dyldhook/src/webkit_dsc_patch.c
@@ -295,3 +295,26 @@ bool HOOK(_ZNK5dyld416JustInTimeLoader11matchesPathEPKc)(const void *self, const
 
     return false;
 }
+
+// ============================================================================
+// Linker Dummy Stubs
+// MachOMerger needs these symbols to exist during the initial clang linking
+// phase. MachOMerger will overwrite these stubs with actual branches later.
+// ============================================================================
+__asm__(
+".globl _MACHOMERGER_ORIG__ZNK5dyld416JustInTimeLoader11matchesPathEPKc\n"
+"_MACHOMERGER_ORIG__ZNK5dyld416JustInTimeLoader11matchesPathEPKc:\n"
+"    ret\n"
+
+".globl _MACHOMERGER_ORIG__ZNK5dyld46Loader17applyCachePatchesERNS_12RuntimeStateERNS_34DyldCacheDataConstLazyScopedWriterE\n"
+"_MACHOMERGER_ORIG__ZNK5dyld46Loader17applyCachePatchesERNS_12RuntimeStateERNS_34DyldCacheDataConstLazyScopedWriterE:\n"
+"    ret\n"
+
+".globl _MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader11loadAddressERNS_12RuntimeStateE\n"
+"_MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader11loadAddressERNS_12RuntimeStateE:\n"
+"    ret\n"
+
+".globl _MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader21overridesDylibInCacheERPKNS0_10DylibPatchERt\n"
+"_MACHOMERGER_TRAMPOLINE__ZNK5dyld46Loader21overridesDylibInCacheERPKNS0_10DylibPatchERt:\n"
+"    ret\n"
+);
